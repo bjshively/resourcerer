0a57d1c9bc2b03bd211029872004fa31ce7f2568
Bradley Shively <bshively@gmail.com>
2015-11-09 02:26:59 -0500

    Implement Resource and Lease models. Print available resources

--- main.py
+++ main.py
@@ -23,30 +23,55 @@ class MainPage(webapp2.RequestHandler):
     user = users.get_current_user()
     login = users.create_login_url('/')
     logout = users.create_logout_url('/')
+    resources = Resource.get_available_resources()
 
     template_values = {
       'user': user,
       'login': login,
-      'logout': logout
+      'logout': logout,
+      'resources': resources
     }
-
+    
+    """
+    res = Resource()
+    res.resType = 'Server'
+    res.title = 'DonkeyKong'
+    res.description = 'An Ubuntu 14.04 server for all your computing needs.'
+    res.availability = 'true'
+    res.put()
+    """
     render_template(self, 'index.html', template_values)
-
-class ErrorPage(webapp2.RequestHandler):
-  def get(self):
-    code = self.request.get('msg')
-    error_msg = 'An unknown error occurred.'
-
-    template_values = {
-      'error_msg': error_msg
-    }
-
-    render_template(self, 'error.html', template_values)
 
 ######################################################################
 # DB objects
 ######################################################################
-class Resource(db.Model)
+class Resource(ndb.Model):
+  """A resource asset that can be leased by a user"""
+  resType = ndb.StringProperty()
+  title = ndb.StringProperty()
+  description = ndb.StringProperty()
+  availability = ndb.StringProperty()
+
+  @classmethod
+  def get_available_resources(cls):
+    # return cls.query(availability == 'true')
+    results = cls.query(cls.availability=='true').fetch()
+    """
+    sortedRes = dict()
+    for res in results:
+      if res.resType not in sortedRes:
+        sortedRes[res.resType] = [res]
+      else:
+        sortedRes[res.resType].append(res)
+    return sortedRes
+    """
+    return cls.query(cls.availability=='true').fetch()
+
+class Lease(ndb.Model):
+  """A lease that grants a user access to a resource for a period of time"""
+  owner = ndb.StringProperty()
+  resource = ndb.StringProperty()
+  
 
 ######################################################################
 # define routes and create the app
